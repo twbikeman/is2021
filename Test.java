380c911b19aa8b35cb60b664e0549e8831cd7d70
Your Name <you@example.com>
2021-03-27 22:52:17 +0800

    finish key

--- Test.java
+++ Test.java
@@ -63,9 +63,33 @@ class Test {
 		return output;
 	}
 
+	public void keyGen(char k, char[] key) {
+		int[]  ip = {BIT2, BIT6, BIT3, BIT1, BIT4, BIT8, BIT5,BIT7};
+		int[] p10 = {BIT3_10, BIT5_10, BIT2_10, BIT7_10, BIT4_10, BIT10_10, BIT1_10, BIT9_10, BIT8_10, BIT6_10};
+		int[] l4_8 = {BIT1, BIT2, BIT3, BIT4};
+		int[] r4_8 = {BIT5, BIT6, BIT7, BIT8};
+		int[] l5_10 = {BIT1_10, BIT2_10, BIT3_10, BIT4_10, BIT5_10};
+		int[] r5_10 = {BIT6_10, BIT7_10, BIT8_10, BIT9_10, BIT10_10};
+		int[] p8 = {BIT6_10, BIT3_10,BIT7_10,BIT4_10, BIT8_10,BIT5_10,BIT10_10,BIT9_10};
+
+		k = permute(k, p10);
+		char k1_l = ls(permute(k, l5_10), 5, 1);
+		char k1_r = ls(permute(k, r5_10), 5, 1);
+		char k2_l = ls(k1_l, 5, 2);
+		char k2_r = ls(k1_r, 5, 2);
+		
+		char k1 = permute(merge(k1_l, k1_r, 5), p8);
+		char k2 = permute(merge(k2_l, k2_r, 5), p8);
+		key[0] = k1;
+		key[1] = k2;
+
+
+	
+	}
+
 	
 	public static void main(String[] argv) {
-		Test t = new Test();
+/*		Test t = new Test();
 		int[]  ip = {BIT2, BIT6, BIT3, BIT1, BIT4, BIT8, BIT5,BIT7};
 		int[] p10 = {BIT3_10, BIT5_10, BIT2_10, BIT7_10, BIT4_10, BIT10_10, BIT1_10, BIT9_10, BIT8_10, BIT6_10};
 		int[] l4_8 = {BIT1, BIT2, BIT3, BIT4};
@@ -80,9 +104,14 @@ class Test {
 		k = t.merge(k1, k2, 5);
 		k = t.permute(k, p8);
 		t.expBin(k, 8);
+*/
+		Test sdes = new Test();
+		char x = 0x031E;
+		char[] key = new char[2];
+		sdes.keyGen(x, key);
 
-		
-
+		sdes.expBin(key[0],8);
+		sdes.expBin(key[1],8);
 	}
 
 }
